d7249f9fc8d9a856a68e5734729cc3e64499ea7b
dylangorman <dylangorman@me.com>
2022-01-06 17:01:24 +0000

    commit3

--- src/app.js
+++ src/app.js
@@ -1,34 +1,25 @@
 const yargs = require("yargs");
-const fs = require("fs");
+
 const {
   addMovie,
   listMovies,
-  updateMovie,
-  deleteMovie,
+  // updateMovie,
+  // deleteMovie,
 } = require("./utils/index");
+const fs = require("fs");
+const { ConnectionCheckedInEvent } = require("mongodb");
 const connection = require("./db/connection");
 const command = process.argv[2];
 const app = async (args) => {
   try {
     if (command === "add") {
       const movieObj = { title: args.title, actor: args.actor };
-      console.log("app.js hit");
+      // console.log("app.js hit");
       await connection(addMovie, movieObj);
-      console.log("after connection");
+      // console.log("after connection");
+    } else if (command === "list") {
+      await connection(listMovies);
     }
-    // } else if (process.argv[2] === "delete") {
-    //   deleteMovie(movieArray, {
-    //     title: yargs.argv.title,
-    //     actor: yargs.argv.actor,
-    //   });
-    // } else if (process.argv[2] === "list") {
-    //   listMovies();
-    // } else if (process.argv[2] === "update") {
-    //   updateMovie(yargs.argv.title, yargs.argv.actor);
-    // }
-    // } else {
-    //   console.log("Input unknown, please try again!");
-    // }
   } catch (error) {
     console.log(error);
   }
--- src/utils/index.js
+++ src/utils/index.js
@@ -8,48 +8,51 @@ const addMovie = async (collection, movieObj) => {
     console.log(error);
   }
 };
-const listMovies = async () => {
+const listMovies = async (collection) => {
   try {
-    console.log(await collection.find({}));
+    const movieArray = await collection.find({}).toArray();
+    if (movieArray.length >= 1) {
+      console.log(movieArray);
+    } else "There are no movies to display.";
   } catch (error) {
     console.log(error);
   }
 };
-const updateMovie = (movieArray, filterObj, newMovie) => {
-  try {
-    let newArray = movieArray;
-    for (movie in newArray) {
-      if (newArray[movie].title === filterObj.title) {
-        newArray[movie] = newMovie;
-        const stringyObj = JSON.stringify(newArray);
-        fs.writeFileSync("./storage.json", stringyObj);
-        break;
-      } else {
-        console.log("I don't understand");
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-const deleteMovie = (movieArray, filterObj) => {
-  try {
-    let newArray = movieArray.filter(
-      (movie) => movie.title !== filterObj.title
-    );
-    const stringyObj = JSON.stringify(newArray);
-    fs.writeFileSync("./storage.json", stringyObj);
-    console.log(
-      "Thank you for your input, this data has now been removed from the list!"
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const updateMovie = (movieArray, filterObj, newMovie) => {
+//   try {
+//     let newArray = movieArray;
+//     for (movie in newArray) {
+//       if (newArray[movie].title === filterObj.title) {
+//         newArray[movie] = newMovie;
+//         const stringyObj = JSON.stringify(newArray);
+//         fs.writeFileSync("./storage.json", stringyObj);
+//         break;
+//       } else {
+//         console.log("I don't understand");
+//       }
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// const deleteMovie = (movieArray, filterObj) => {
+//   try {
+//     let newArray = movieArray.filter(
+//       (movie) => movie.title !== filterObj.title
+//     );
+//     const stringyObj = JSON.stringify(newArray);
+//     fs.writeFileSync("./storage.json", stringyObj);
+//     console.log(
+//       "Thank you for your input, this data has now been removed from the list!"
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 module.exports = {
   addMovie,
   listMovies,
-  updateMovie,
-  deleteMovie,
+  // updateMovie,
+  // deleteMovie,
 };
